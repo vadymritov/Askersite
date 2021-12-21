0dd91da1db4630753b0236e4dbad36618084d50f
Nataly <vozzna11@gmail.com>
2021-12-21 17:52:19 +0200

    animation

--- src/container/Home/AskerHomeStatic.js
+++ src/container/Home/AskerHomeStatic.js
@@ -4,26 +4,19 @@ import styles from './Home.module.scss'
 const AskerHomeStatic = (props) => {
   return (
     <div className={styles.askerWrapper} id="homeTiltWrapper">
-      {/*<div className="home-tilt-wrapper hasHomePage" id="homeTiltWrapper">*/}
-      {/*    <div className="container">*/}
       <div className={`${styles.askerRow}`}>
         <div className={`${styles.askerCol}`}>
-          <div className={`home-tilt-inner rgt ${styles.imgWrap}`}>
+          <div className={`${styles.imgWrap}`}>
             <div className={styles.shadow}/>
             <div className={styles.listingImg}>
               <div className={styles.iconImg}/>
             </div>
-            {/*<div className="img mscreen-img">*/}
-            {/*<img alt="" src={tilt02} />*/}
-            {/*</div>*/}
-            {/*<div className{`${styles.listingImg}`}>*/}
-            {/*<div className{`img pos-abs listing-img tilt-js ${styles.listingImg}`}>*/}
-            {/*<img alt="" src={tilt05} />*/}
-            {/*</div>*/}
           </div>
         </div>
-        <div className={`col col-md-6 home-tilt-col ${styles.askerCol}`}>
-          <div className="home-tilt-inner lft">
+        <div className={`${styles.askerCol}`}>
+          <div className={styles.imgWrapStatic}>
+
+          {/*<div className="home-tilt-inner lft">*/}
             {/*<div className="img-wrapper">*/}
             {/*    <div className="img mscreen-img">*/}
             {/*        <img alt="" src={tilt01} />*/}
