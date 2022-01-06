0dfc35969834de03097e4357377a977fdbe7f14e
Alexeygmr <alexey@cravter.com>
2022-01-06 15:57:01 +0200

    add changes for Settings.js window, add animation for Settings.js.

--- src/components/BurgerHeader/BurgerHeader.js
+++ src/components/BurgerHeader/BurgerHeader.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { slide as Menu } from 'react-burger-menu'
+import {slide as Menu} from 'react-burger-menu'
 import styles from './BurgerHeader.module.scss'
 import {NavLink} from "react-router-dom";
 
@@ -39,21 +39,22 @@ const BurgerHeader = (props) => {
     closeSideBar()
   }
   return (
-    <Menu {...props} height={'100%'}  className={styles.burgerMenu} isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}  >
-      <div className={styles.containerBurgerMenu} >
-        <div className={styles.tabBoxBurgerMenu} >
+    <Menu {...props} height={'100%'} className={styles.burgerMenu} isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}>
+      <div className={styles.containerBurgerMenu}>
+        <div
+          className={styles.tabBoxBurgerMenu}>
           {
             blockLinks.map((link, index) =>
-              <NavLink key={index} className={styles.linkWrapBurgerMenu} to={link.href} activeclassname={styles.active} onClick={()=>closeSideBar()} >
+              <NavLink key={index} className={styles.linkWrapBurgerMenu} to={link.href} activeclassname={styles.active} onClick={() => closeSideBar()}>
                 <span className={styles.itemLinkBurgerMenu}>{link.text}</span>
               </NavLink>
             )
           }
           {!token ? <NavLink className={styles.joinWrapBurgerMenu} to={'/sign-up'} activeclassname={styles.active}>
             <span className={styles.joinLinkBurgerMenu}>Join</span>
-          </NavLink>  :  <NavLink className={styles.joinWrapBurgerMenu} to={'/logout'} activeclassname={styles.active} onClick={logoutFn} >
+          </NavLink> : <NavLink className={styles.joinWrapBurgerMenu} to={'/logout'} activeclassname={styles.active} onClick={logoutFn}>
             <span className={styles.joinLinkBurgerMenu}>Logout</span>
-          </NavLink> }
+          </NavLink>}
         </div>
       </div>
 
