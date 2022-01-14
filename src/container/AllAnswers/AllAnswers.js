3c59ee8b38511bb2345abb247633c0e3223eb859
Alexeygmr <alexey@cravter.com>
2022-01-14 17:06:09 +0200

    add functionality for button 'watch' and navigate to watch-answer.

--- src/container/AllAnswers/AllAnswers.js
+++ src/container/AllAnswers/AllAnswers.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './AllAnswers.module.scss'
 import AllAnswerIcon from "../../components/UI/icons/AllAnswerIcon";
+import {useNavigate} from "react-router-dom";
+import {http} from "../../http/http";
 
 const AllAnswers = (props) => {
+  let navigate = useNavigate();
+  const [arrAsker, setArrAsker] = useState([])
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,]
+  const userID = JSON.parse(localStorage.getItem("UserID"));
+  useState(()=>{
+    http.post('allAsker', `user_id=${userID}`).then(resp => resp.data).then(askersData => setArrAsker(askersData.asker.slice(0,16)))
+  },[])
 
   return (
     <div className={styles.mainContainer}>
@@ -13,8 +21,11 @@ const AllAnswers = (props) => {
       </div>
       <div className={styles.row}>
       {
-        arr.map((item, index) => {
-          return <div key={index} className={styles.cardWrap}>
+        arrAsker.map((item, index) => {
+          return <div key={item.asker_id} onClick={()=>navigate('/watch-answer',{state:{
+            asker_id:item.asker_id,
+              user_id:userID
+            }})} className={styles.cardWrap}>
             <div className={styles.answersContainer}>
               <div className={styles.answerCard}/>
             </div>
