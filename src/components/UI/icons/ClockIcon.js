5f5fb4693c08cbc24520d24cdf301e957d0a9422
Nataly <vozzna11@gmail.com>
2022-01-17 16:55:34 +0200

    fixed The 'introduce yourself... and 'Tell me what makes''' floating windows should have the same transparency+blur effect

--- src/components/UI/icons/ClockIcon.js
+++ src/components/UI/icons/ClockIcon.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ClockIcon = ({className}) => (
   <svg className={className} viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01", stroke: "#ADB8FA", strokeWidth: "1.41935"}} d="M21.9999 14.8441C25.9522 14.8441 29.1558 18.0478 29.1558 22C29.1558 25.9522 25.9522 29.1559 21.9999 29.1559C18.0479 29.1559 14.844 25.9522 14.844 22C14.844 18.0478 18.0479 14.8441 21.9999 14.8441Z" />
-    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01", stroke: "#ADB8FA", strokeWidth: "0.709677"}} d="M21.9999 22.0002H21.3494V17.4464H21.9999V22.0002Z" />
-    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01", stroke: "#ADB8FA", strokeWidth: "0.709677"}} d="M25.3988 23.6606L25.1078 24.2424L21.2046 22.2909L21.4956 21.7089L25.3988 23.6606Z"/>
+    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01", strokeWidth: "1.41935"}} d="M21.9999 14.8441C25.9522 14.8441 29.1558 18.0478 29.1558 22C29.1558 25.9522 25.9522 29.1559 21.9999 29.1559C18.0479 29.1559 14.844 25.9522 14.844 22C14.844 18.0478 18.0479 14.8441 21.9999 14.8441Z" />
+    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01",strokeWidth: "0.709677"}} d="M21.9999 22.0002H21.3494V17.4464H21.9999V22.0002Z" />
+    <path style={{fillRule: "evenodd", clipRule: "evenodd", fill: "black", fillOpacity: "0.01",  strokeWidth: "0.709677"}} d="M25.3988 23.6606L25.1078 24.2424L21.2046 22.2909L21.4956 21.7089L25.3988 23.6606Z"/>
   </svg>
 );
 
--- src/container/Home/HomeStart.js
+++ src/container/Home/HomeStart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Home.module.scss'
+import ClockIcon from "../../components/UI/icons/ClockIcon";
 
 const HomeStart = (props) => {
 
@@ -17,11 +18,35 @@ const HomeStart = (props) => {
         </div>
         <div className={`${styles.askerCol}`}>
           <div className={styles.question}>
-            <div className={styles.iconTop}/>
+            {/*<div className={styles.iconTop}/>*/}
+            <div className={`${styles.floatingBoxTop}`}>
+              <div className={`${styles.floatingBottomContent}`}>
+                <div className={`${styles.circleBox}`}/>
+                <div className={`${styles.floatingText}`}>Tell me what makes you perfect for this role</div>
+                <div className={styles.iconsBox}>
+                  <ClockIcon className={styles.iconClock}/>
+                  <span>30s</span>
+                </div>
+              </div>
+
+            </div>
           </div>
           <div className={styles.imgWrapStatic}/>
+          {/*<div className={`${styles.questionBottom}`}>*/}
+          {/*  <div className={`${styles.iconBottom}`}/>*/}
+          {/*</div>*/}
           <div className={`${styles.questionBottom}`}>
-            <div className={`${styles.iconBottom}`}/>
+            {/*<div className={`${styles.iconBottom}`}/>*/}
+            <div className={`${styles.floatingBoxBottom}`}>
+              <div className={`${styles.floatingBottomContent}`}>
+                <div className={`${styles.circleBox}`}/>
+                <div className={`${styles.floatingText}`}>Introduce yourself in 60 seconds or less</div>
+                <div className={styles.iconsBox}>
+                  <ClockIcon className={styles.iconClock}/>
+                  <span>30s</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
