167cfc723101bda397d828875b2f7bfd02d85e99
tomgroup <conda0905@gmail.com>
2022-02-02 10:06:18 +0800

    Milestone #1
    Home page
    - Large text on the right entered vertically and style/colour etc should be same as figma
    - Large text on the right should pause for 2 seconds when complete. The backspacing (deleteing animation) should be faster (1 second)
    - 2 green and blue floating ? Icons on the right hand side should be 70% of current size
    - ‘Create Asker’ Button should use spin effect to change to the ‘Answer Asker’ button as shown on ‘Home Screen 2’ page on figma
    - 2 floating phone screens on the left floating distance reduced by 59%
    
    - Replace two floating rectangles with questions on left hand side with new ones. Make sure they have exact same shadow / transparency with background blur) effect as figma
    - On Transition from Home Screen to ‘Login’,
    - Use correct shadows on left hand side phone graphics
    - Replace inside of blue phone graphic with correct graphic
    - Add functionality to ‘back’ buttons

--- src/container/RightScreen/RightScreen.js
+++ src/container/RightScreen/RightScreen.js
@@ -9,6 +9,7 @@ import {useLocation, useNavigate} from "react-router-dom";
 const RightScreen = (props) => {
   const {pathname} = useLocation();
   const el = useRef(null);
+  const [askertext, setAskerText] = React.useState("Create Asker");
   let navigate = useNavigate();
   let token = localStorage.getItem('UserID');
 
@@ -16,20 +17,20 @@ const RightScreen = (props) => {
     const typed = new Typed(el.current, {
       strings: [
         'Tell me about <br/> yourself in <br/> <span class="activeTypedViolet">30 seconds</span>',
-        'Tell me what <br/> makes you <span class="activeTyped">perfect</span> <br/> for this role',
+        'Tell me what <br/> makes you <span class="activeTyped">perfect</span> for this role',
         'Tell me what <br/> makes you <br/> <span class="activeTypedViolet">unique</span>',
-        'Tell me about <br/> your <span class="activeTyped">strengths</span> <span class="use-and">and</span> <br/> <span class="activeTyped">weaknesses</span>',
-        'Tell me what <br/> you most enjoy <br/> about <span class="activeTypedViolet">work</span>',
+        'Tell me about <br/> your <span class="activeTyped">strengths</span> and <span class="activeTyped">weaknesses</span>',
+        'Tell me what <br/> you most <span class="activeTyped">enjoy</span> about work',
         'Tell me about <br/> your relevant <br/><span class="activeTyped">experience</span>',
         'Tell me what <br/> makes you a great <br/><span class="activeTypedViolet">candidate</span>',
         'Tell me where <br/> you see yourself<br/> in <span class="activeTyped"> 5 years</span>',
         'Tell me about <br/> a time you face <br/> <span class="activeTypedViolet">adversity</span>',
-        'Tell me what <br/> you’d like to <span class="activeTyped">ask</span> about <br/> the position'
+        'Tell me what <br/> you’d like to <span class="activeTyped">ask</span> about the position'
       ],
       startDelay: 1000,
       typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 100,
+      backSpeed: 35,
+      backDelay: 2000,
       smartBackspace: true,
       loop: true,
       showCursor: true,
@@ -62,7 +63,12 @@ const RightScreen = (props) => {
             </h2>
           </div>
           <div className={styles.btnLine}>
-            <button type="button" className={styles.btnCreate} onClick={onCreateAsker}>Create Asker</button>
+            <button type="button" className={styles.btnCreate} onClick={onCreateAsker} 
+              onMouseOver={()=>setAskerText('Answer Asker')}
+              onMouseLeave={()=>setAskerText('Create Asker')}
+            >
+              {askertext}
+            </button>
           </div>
           <div className={styles.wrapIcon}>
           <QuestionViolet className={styles.violetIcon}/>
